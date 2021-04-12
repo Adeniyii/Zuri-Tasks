c128764347ca50968bd66a6540d9ecef8776181f
Ifedayo Ijabadeniyi <ifedayoadeniyi@gmail.com>
2021-04-12 19:27:27 +0100

    Complete program 🍖

---
+++ main.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const axios = require("axios");
+
+const URL = "http://jsonplaceholder.typicode.com/posts/1";
+
+(async function main() {
+  const data = await fetchData(URL);
+  const written = await writeToFile(data);
+
+  console.log(written ?? "Something went wrong, please contact the developer.");
+})();
+
+async function fetchData(url) {
+  try {
+    const rawData = await axios(url);
+    if (typeof rawData === "undefined") {
+      throw new Error("Unable to fetch resource.");
+    }
+    return rawData;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function writeToFile(result) {
+  const dataString = JSON.stringify(result.data);
+
+  fs.writeFile("./results/posts.txt", dataString, (err) => {
+    if (err) throw err;
+  });
+  return "Data was written to file successfully.";
+}
