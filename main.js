b0a61db9d2a5ee02048bf7f3372078e0b578d761
Ifedayo Ijabadeniyi <ifedayoadeniyi@gmail.com>
2021-04-21 20:26:01 +0100

    Check if result folder exists, and edit JSON format 🍕

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-const fs = require("fs");
 const axios = require("axios");
+const { join } = require("path");
+const { mkdir, writeFile } = require("fs").promises;
 
 const URL = "http://jsonplaceholder.typicode.com/posts/";
 
-// Start program
-(async function main() {
+async function main() {
   const data = await fetchData(URL);
   const written = await writeToFile(data);
 
-  console.log(written ?? "Something went wrong, please contact the developer.");
-})();
+  console.log(
+    written ?? "Something went wrong, please contact the developer..."
+  );
+}
 
 async function fetchData(url) {
   try {
     const rawData = await axios(url);
     if (!rawData) {
       // If nothing was returned from resource
-      throw new Error("Unable to fetch resource.");
+      throw new Error("Unable to fetch resource...");
     }
     return rawData;
   } catch (err) {
@@ -24,14 +26,31 @@ async function fetchData(url) {
   }
 }
 
-async function writeToFile(result) {
-  // fs.writeFile() requires strings
-  const dataString = JSON.stringify(result.data);
+async function createDir(path) {
+  try {
+    await mkdir(path);
+    console.log("created result directory...");
+  } catch (err) {
+    if (err.code === "EEXIST") return null;
+    else return err;
+  }
+}
 
-  fs.writeFile("./results/posts.json", dataString, (err) => {
-    if (err) throw err;
-  });
+async function writeToFile(result) {
+  const filePath = join(__dirname, "result");
+  const dataString = JSON.stringify({ ...result.data });
+
+  try {
+    const didNotWork = await createDir(filePath);
+
+    if (didNotWork) throw didNotWork;
+    else await writeFile("./result/posts.json", dataString);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
   return "Data was written to file successfully.";
 }
 
-// Victory!
+main();
